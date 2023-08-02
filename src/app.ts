943495af9995545f06f653140423708a80e9a17b
caua.lemos <caua.lemos@movemais.com>
2023-08-01 21:09:05 -0300

    fix: rmv size limit

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,8 @@ import appMiddleware from "./application/middlewares/app-middleware"
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors)
 app.use(rateLimit)
 app.use(router)
